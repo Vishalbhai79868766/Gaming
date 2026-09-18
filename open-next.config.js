72fd88329143faa58be5984a14d6280597384f6b
Sodha Vishal <vishalbhaisodha48@gmail.com>
2026-09-18 19:18:04 +0100

    Add open-next configuration for Cloudflare Edge

---
+++ open-next.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+     default: {
+       override: {
+         wrapper: "cloudflare-node",
+         converter: "edge",
+         proxyExternalRequest: "fetch",
+       },
+     },
+     edgeExternals: ["node:crypto"],
+     middleware: {
+       external: true,
+       override: {
+         wrapper: "cloudflare-edge",
+         converter: "edge",
+         proxyExternalRequest: "fetch",
+       },
+     },
+   };
